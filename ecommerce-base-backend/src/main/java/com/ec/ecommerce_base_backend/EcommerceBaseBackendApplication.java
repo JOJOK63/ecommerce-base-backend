f5897eecabd4c2068c86063b6b4de6316ba7cd8b
JOJOK <krariajordan@gmail.com>
2025-02-04 14:10:25 +0100

    starter

---
+++ ecommerce-base-backend/src/main/java/com/ec/ecommerce_base_backend/EcommerceBaseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ec.ecommerce_base_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceBaseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceBaseBackendApplication.class, args);
+	}
+
+}
---
+++ ecommerce-base-backend/src/test/java/com/ec/ecommerce_base_backend/EcommerceBaseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ec.ecommerce_base_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceBaseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
