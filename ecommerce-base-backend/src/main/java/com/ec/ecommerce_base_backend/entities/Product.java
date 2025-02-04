e79cc8739e7ce815be86dc55056a42bf7bc519bd
JOJOK <krariajordan@gmail.com>
2025-02-04 16:21:25 +0100

    product

---
+++ ecommerce-base-backend/src/main/java/com/ec/ecommerce_base_backend/entities/Product.java
@@ -0,0 +1,63 @@
+package com.ec.ecommerce_base_backend.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDateTime;
+
+
+@Data   // create automatically getters, setters, toString, equals et hashCode
+@NoArgsConstructor  // generate constructor whitout args
+@AllArgsConstructor //  generate constructor whitout args
+@Builder // instanciation automaticly withouth ... new Object
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+
+    @Column
+    String name;
+
+    @Column
+    String description;
+
+    @Column
+    float price;
+
+    @Column
+    int stock;
+
+    @Column
+    float weight;
+
+    @Column
+    LocalDateTime createdAt;
+
+    @Column
+    LocalDateTime updatedAt;
+
+    Status status;
+
+    /*
+    * Images
+    * Category
+    * Comments
+    * Rating
+    * */
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
---
+++ ecommerce-base-backend/src/main/java/com/ec/ecommerce_base_backend/entities/Status.java
@@ -0,0 +1,7 @@
+package com.ec.ecommerce_base_backend.entities;
+
+public enum Status {
+    AVAILABLE,
+    OUT_OF_STOCK,
+    INDISPONIBILITY
+}
